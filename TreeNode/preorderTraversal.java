06c13a450411e1e4bea9db5e795bb073f082352c
littleBird9018 <453045096@qq.com>
2022-10-25 10:30:19 +0800

    二叉树的前序遍历-迭代版

---
+++ TreeNode/preorderTraversal.java
@@ -0,0 +1,21 @@
+/**
+ * @author nan
+ * @date 2022/10/24 16:07
+ * @Description 二叉树的前序遍历-迭代版
+ */
+public class MyCircularQueue {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            ans.add(node.val);
+            if(node.right != null) stack.push(node.right);
+            if(node.left != null) stack.push(node.left);
+        }
+        return ans;
+    }
+}
