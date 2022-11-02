d43c6b2142af64639b0092517040787b50d0efe8
littleBird9018 <453045096@qq.com>
2022-11-02 21:53:34 +0800

    寻找重复的子树

---
+++ TreeNode/findDuplicateSubtrees.java
@@ -0,0 +1,29 @@
+/**
+ * @author nan
+ * @date 2022/10/24 16:07
+ * @Description 寻找重复的子树-递归版
+ */
+public class constructFromPrePost {
+
+    //time: O(n2)
+    //space: O(n)
+    List<TreeNode> ans = new ArrayList<>();
+    HashMap<String,Integer> map = new HashMap<>();
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        duplicateTree(root);
+        return ans;
+    }
+
+    public TreeNode duplicateTree(TreeNode node){
+        if(node == null) return null;
+        TreeNode left = duplicateTree(node.left);
+        TreeNode right = duplicateTree(node.right);
+        String key = node.val + "-" + (left == null ? "201" : left.val) + "-" + (right == null ? "202" : right.val);
+        map.put(key,map.getOrDefault(key,0) + 1);
+        System.out.println(map);
+        if(map.get(key) == 2){
+            ans.add(node);
+        }
+        return node;
+    }
+}
