b1a8a1999fc0ab1b039d7120d1ef3855009d14a0
littleBird9018 <453045096@qq.com>
2022-11-06 21:28:41 +0800

    Goal Parser Interpretation

---
+++ Data Structure Design/Goal Parser Interpretation.java
@@ -0,0 +1,31 @@
+/**
+ * @author nan
+ * @date 2022/10/24 16:07
+ * @Description 设计 Goal 解析器
+ */
+public class interpret {
+
+    public String interpret(String command) {
+        StringBuilder stb = new StringBuilder();
+        char[] chars = command.toCharArray();
+        for(int i = 0; i < chars.length;i++){
+            switch(chars[i]){
+                case 'G':
+                    stb.append("G");
+                    break;
+                case '(':
+                    i++;
+                    if(chars[i] == ')'){
+                        stb.append("o");
+                    }else{
+                        stb.append("al");
+                        i += 2;
+                    }
+                    break;
+                default:break;
+                    
+            }
+        }
+        return stb.toString();
+    }
+}
