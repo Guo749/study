8f312e07f1c47235ddc7268634d86b0ef11f9193
littleBird9018 <453045096@qq.com>
2022-11-09 23:22:56 +0800

    用队列实现栈-用一个队列实现栈

---
+++ Data Structure Design/MyStack.java
@@ -0,0 +1,31 @@
+/**
+ * @author nan
+ * @date 2022/10/24 16:07
+ * @Description 用队列实现栈-用一个队列实现栈
+ */
+public class MyStack {
+    Deque<Integer> queue;
+    public MyStack() {
+        queue = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        int size = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < size; i++) {
+            queue.offer(queue.poll());
+        }
+    }
+    
+    public int pop() {      
+        return queue.poll();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
